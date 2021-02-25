504853c80365aaea21eb4b548480ab87f96256c8
Enes Malik Özer <enesmozer@gmail.com>
2021-02-25 12:53:03 +0300

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const prefix = require('gulp-autoprefixer')
+const plumber = require('gulp-plumber')
+const reload = browserSync.reload
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './'
+    }
+  })
+  gulp.watch('./*.html').on('change', reload)
+  gulp.watch('./scss/**/*.scss', ['css'])
+})
+
+gulp.task('css', () => {
+  return gulp.src('./scss/main.scss')
+  .pipe(plumber([{errorHandler: false}]))
+  .pipe(sass())
+  .pipe(prefix())
+  .pipe(gulp.dest('./'))
+  .pipe(browserSync.stream())
+})
+gulp.task('default', ['browser-sync', 'css'])
